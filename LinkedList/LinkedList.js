3cfe55594b5417a916298edf8a0c8ab889374a7b
Gordon Gao <YuanGao0317@users.noreply.github.com>
2016-09-13 19:42:12 -0400

    Create LinkedList.js

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,43 @@
+function Node(value) {
+    this.data = value;
+    this.previous = null;
+    this.next = null;
+}
+ 
+function LinkedList() {
+    this._length = 0;
+    this.head = null;
+    this.tail = null;
+}
+
+LinkedList.prototype.add = function(value) {
+    var node = new Node(value);
+ 
+    if (this._length) {
+        this.tail.next = node;
+        node.previous = this.tail;
+        this.tail = node;
+    } else {
+        this.head = node;
+        this.tail = node;
+    }
+ 
+    this._length++;
+ 
+    return node;
+};
+
+LinkedList.prototype.deleteDups = function(n){
+	var holder = new Set();
+	var previous;
+	
+	while(n !== null) {
+		if (holder.has(n.data)) {
+			previous.next = n.next;
+		} else {
+			holder.add(n.data);
+			previous = n;
+		}
+		n = n.next;
+	}
+}
